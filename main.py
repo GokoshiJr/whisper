e3a04c8180da2f0b2bd89ae384cdeb185558467e
GokoshiJr <julioagonzalez18@gmail.com>
2024-04-12 09:40:47 -0400

    Remove flags

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from whisper.utils import get_writer
 try:
     start = time.perf_counter()
     model = whisper.load_model("tiny")
-    result = model.transcribe("audio/test.mp3", fp16=False, language="es", initial_prompt="prompt", word_timestamps=True)
+    result = model.transcribe("audio/test.mp3", fp16=False, language="es")
     writer = get_writer(output_format="all", output_dir="target")
     writer(result, "audio/test.mp3")
     end = time.perf_counter() - start
