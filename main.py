68c840db1ce18009abe79935c0de2c8697436dd2
GokoshiJr <julioagonzalez18@gmail.com>
2024-04-12 09:38:07 -0400

    Add get_writer

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import whisper
+import whisper, time
+from whisper.utils import get_writer
 
-model = whisper.load_model("tiny")
-result = model.transcribe("audio/test.mp3", fp16=False, language="es")
-print(result["text"])
+try:
+    start = time.perf_counter()
+    model = whisper.load_model("tiny")
+    result = model.transcribe("audio/test.mp3", fp16=False, language="es", initial_prompt="prompt", word_timestamps=True)
+    writer = get_writer(output_format="all", output_dir="target")
+    writer(result, "audio/test.mp3")
+    end = time.perf_counter() - start
+    print("Tiempo de ejecucion: ", end)
+except Exception as e:
+    print(e.with_traceback())
